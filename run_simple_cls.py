45b482cc8d0f73fe8d9c663805fe6f9d22a67f4e
cheungbh <534660436@qq.com>
2023-09-22 10:16:43 +0800

    add simple version of inference

---
+++ run_simple_cls.py
@@ -0,0 +1,166 @@
+
+from common.arguments import parse_args
+import os
+from common.camera import *
+from common.model import *
+from common.loss import *
+from common.generators import UnchunkedGenerator
+
+
+args = parse_args()
+print(args)
+print('Loading dataset...')
+from common.custom_dataset import CustomDataset
+
+dataset = CustomDataset('data/data_2d_' + args.dataset + '_' + args.keypoints + '.npz')
+print('Preparing data...')
+for subject in dataset.subjects():
+    for action in dataset[subject].keys():
+        anim = dataset[subject][action]
+
+        if 'positions' in anim:
+            positions_3d = []
+            for cam in anim['cameras']:
+                pos_3d = world_to_camera(anim['positions'], R=cam['orientation'], t=cam['translation'])
+                pos_3d[:, 1:] -= pos_3d[:, :1]  # Remove global offset, but keep trajectory in first position
+                positions_3d.append(pos_3d)
+            anim['positions_3d'] = positions_3d
+
+print('Loading 2D detections...')
+keypoints = np.load('data/data_2d_' + args.dataset + '_' + args.keypoints + '.npz', allow_pickle=True)
+keypoints_metadata = keypoints['metadata'].item()
+keypoints_symmetry = keypoints_metadata['keypoints_symmetry']
+kps_left, kps_right = list(keypoints_symmetry[0]), list(keypoints_symmetry[1])
+joints_left, joints_right = list(dataset.skeleton().joints_left()), list(dataset.skeleton().joints_right())
+keypoints = keypoints['positions_2d'].item()
+
+for subject in keypoints.keys():
+    for action in keypoints[subject]:
+        for cam_idx, kps in enumerate(keypoints[subject][action]):
+            cam = dataset.cameras()[subject][cam_idx]
+            kps[..., :2] = normalize_screen_coordinates(kps[..., :2], w=cam['res_w'], h=cam['res_h'])
+            keypoints[subject][action][cam_idx] = kps
+
+subjects_train = args.subjects_train.split(',')
+subjects_semi = [] if not args.subjects_unlabeled else args.subjects_unlabeled.split(',')
+
+subjects_test = [args.viz_subject]
+
+
+def fetch(subjects, action_filter=None, subset=1, parse_3d_poses=True):
+    out_poses_3d = []
+    out_poses_2d = []
+    out_camera_params = []
+    for subject in subjects:
+        for action in keypoints[subject].keys():
+
+            poses_2d = keypoints[subject][action]
+            for i in range(len(poses_2d)):  # Iterate across cameras
+                out_poses_2d.append(poses_2d[i])
+
+            if subject in dataset.cameras():
+                cams = dataset.cameras()[subject]
+                assert len(cams) == len(poses_2d), 'Camera count mismatch'
+                for cam in cams:
+                    if 'intrinsic' in cam:
+                        out_camera_params.append(cam['intrinsic'])
+
+            if parse_3d_poses and 'positions_3d' in dataset[subject][action]:
+                poses_3d = dataset[subject][action]['positions_3d']
+                assert len(poses_3d) == len(poses_2d), 'Camera count mismatch'
+                for i in range(len(poses_3d)):  # Iterate across cameras
+                    out_poses_3d.append(poses_3d[i])
+
+    if len(out_camera_params) == 0:
+        out_camera_params = None
+    if len(out_poses_3d) == 0:
+        out_poses_3d = None
+
+    return out_camera_params, out_poses_3d, out_poses_2d
+
+
+cameras_valid, poses_valid, poses_valid_2d = fetch(subjects_test)
+
+filter_widths = [int(x) for x in args.architecture.split(',')]
+
+model_pos = TemporalModel(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1], dataset.skeleton().num_joints(),
+                          filter_widths=filter_widths, causal=args.causal, dropout=args.dropout, channels=args.channels,
+                          dense=args.dense)
+
+receptive_field = model_pos.receptive_field()
+print('INFO: Receptive field: {} frames'.format(receptive_field))
+pad = (receptive_field - 1) // 2
+causal_shift = 0
+
+model_params = 0
+for parameter in model_pos.parameters():
+    model_params += parameter.numel()
+print('INFO: Trainable parameter count:', model_params)
+
+if torch.cuda.is_available():
+    model_pos = model_pos.cuda()
+
+if args.resume or args.evaluate:
+    chk_filename = os.path.join(args.checkpoint, args.resume if args.resume else args.evaluate)
+    print('Loading checkpoint', chk_filename)
+    checkpoint = torch.load(chk_filename, map_location=lambda storage, loc: storage)
+    print('This model was trained for ')
+    model_pos.load_state_dict(checkpoint['model_pos'])
+    model_traj = None
+
+test_generator = UnchunkedGenerator(cameras_valid, poses_valid, poses_valid_2d,
+                                    pad=pad, causal_shift=causal_shift, augment=False,
+                                    kps_left=kps_left, kps_right=kps_right, joints_left=joints_left,
+                                    joints_right=joints_right)
+print('INFO: Testing on {} frames'.format(test_generator.num_frames()))
+
+
+if args.render:
+    print('Rendering...')
+
+    input_keypoints = keypoints[args.viz_subject][args.viz_action][args.viz_camera].copy()
+    ground_truth = None
+    gen = UnchunkedGenerator(None, None, [input_keypoints],
+                             pad=pad, causal_shift=causal_shift, augment=args.test_time_augmentation,
+                             kps_left=kps_left, kps_right=kps_right, joints_left=joints_left, joints_right=joints_right)
+
+    with torch.no_grad():
+        model_pos.eval()
+        N = 0
+        for _, batch, batch_2d in gen.next_epoch():
+            inputs_2d = torch.from_numpy(batch_2d.astype('float32'))
+            if torch.cuda.is_available():
+                inputs_2d = inputs_2d.cuda()
+
+            predicted_3d_pos = model_pos(inputs_2d)
+
+            if gen.augment_enabled():
+                predicted_3d_pos[1, :, :, 0] *= -1
+                predicted_3d_pos[1, :, joints_left + joints_right] = predicted_3d_pos[1, :,
+                                                                     joints_right + joints_left]
+                predicted_3d_pos = torch.mean(predicted_3d_pos, dim=0, keepdim=True).squeeze(0).cpu().numpy()
+
+    # prediction = evaluate(gen, return_predictions=True)
+
+    if args.viz_output is not None:
+        cam = dataset.cameras()[args.viz_subject][args.viz_camera]
+        for subject in dataset.cameras():
+            if 'orientation' in dataset.cameras()[subject][args.viz_camera]:
+                rot = dataset.cameras()[subject][args.viz_camera]['orientation']
+                break
+        prediction = camera_to_world(predicted_3d_pos, R=rot, t=0)
+        prediction[:, :, 2] -= np.min(prediction[:, :, 2])
+
+        anim_output = {'Reconstruction': prediction}
+        if ground_truth is not None and not args.viz_no_ground_truth:
+            anim_output['Ground truth'] = ground_truth
+
+        input_keypoints = image_coordinates(input_keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
+
+        from common.visualization import render_animation
+
+        render_animation(input_keypoints, keypoints_metadata, anim_output,
+                         dataset.skeleton(), dataset.fps(), args.viz_bitrate, cam['azimuth'], args.viz_output,
+                         limit=args.viz_limit, downsample=args.viz_downsample, size=args.viz_size,
+                         input_video_path=args.viz_video, viewport=(cam['res_w'], cam['res_h']),
+                         input_video_skip=args.viz_skip)
